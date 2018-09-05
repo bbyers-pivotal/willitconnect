6db412026542bfadf1ab33f983e19a474cb3f05c
Brian Byers <brian@xclo.io>
2018-09-05 13:50:53 -0500

    troubleshooting

--- ci/tasks/checkmarx.sh
+++ ci/tasks/checkmarx.sh
@@ -3,8 +3,8 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd git-repo
-/checkmarx/runCxConsole.sh Scan -v -cxserver $host -projectname $project -cxuser $username -cxpassword $password -locationtype $locationType -locationpath $locationPath
 echo $host
 echo $project
 echo $username
 echo $password
+/checkmarx/runCxConsole.sh Scan -v -cxserver $host -projectname $project -cxuser $username -cxpassword $password -locationtype $locationType -locationpath $locationPath
