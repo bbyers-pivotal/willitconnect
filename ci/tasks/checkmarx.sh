aaeb8aedb029b966109bc466644c3d17ad3140d2
Brian Byers <brian@xclo.io>
2018-09-05 13:52:27 -0500

    removing troubleshooting code

--- ci/tasks/checkmarx.sh
+++ ci/tasks/checkmarx.sh
@@ -3,8 +3,4 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd git-repo
-echo $host
-echo $project
-echo $username
-echo $password
 /checkmarx/runCxConsole.sh Scan -v -cxserver $host -projectname $project -cxuser $username -cxpassword $password -locationtype $locationType -locationpath $locationPath
