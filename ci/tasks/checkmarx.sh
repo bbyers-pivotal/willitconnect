46c38ad80da9bcd3e50b094421b622596fe9eb46
Brian Byers <brian@xclo.io>
2018-09-05 13:07:02 -0500

    adding other placeholders

--- ci/tasks/checkmarx.sh
+++ ci/tasks/checkmarx.sh
@@ -3,6 +3,16 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd git-repo
-#/checkmarx/runCxConsole.sh Scan -v -CxServer <host> -projectName <projectName> -CxUser <username> -CxPassword <password> -Locationtype <Locationtype> -locationpath <locationpath> -Preset <preset> -EnableOsa -OsaLocationPath <filename>
+# /checkmarx/runCxConsole.sh Scan -v -CxServer $host -projectName $project -CxUser $username -CxPassword $password -Locationtype <Locationtype> -locationpath <locationpath> -EnableOsa -OsaLocationPath <filename>
+echo "host"
+echo $host
+echo "username"
+echo $username
 echo "Password passed in"
 echo $password
+echo "project"
+echo $project
+echo "locationType"
+echo $locationType
+echo "locationpath"
+echo $locationpath
