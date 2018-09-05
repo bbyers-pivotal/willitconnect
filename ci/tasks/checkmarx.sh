5dfe7fac38ded1b299126cf03813a140fe0b16f0
Brian Byers <brian@xclo.io>
2018-09-05 13:10:32 -0500

    adding call

--- ci/tasks/checkmarx.sh
+++ ci/tasks/checkmarx.sh
@@ -3,16 +3,4 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd git-repo
-# /checkmarx/runCxConsole.sh Scan -v -CxServer $host -projectName $project -CxUser $username -CxPassword $password -Locationtype <Locationtype> -locationpath <locationpath> -EnableOsa -OsaLocationPath <filename>
-echo "host"
-echo $host
-echo "username"
-echo $username
-echo "Password passed in"
-echo $password
-echo "project"
-echo $project
-echo "locationType"
-echo $locationType
-echo "locationpath"
-echo $locationpath
+/checkmarx/runCxConsole.sh Scan -v -CxServer $host -projectName $project -CxUser $username -CxPassword $password -Locationtype $locationType -locationpath $locationPath
