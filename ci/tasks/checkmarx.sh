fbd9413524e269edfc9940ca54bcfe03a064b406
Brian Byers <brian@xclo.io>
2018-09-06 11:04:54 -0500

    getting absolute path

--- ci/tasks/checkmarx.sh
+++ ci/tasks/checkmarx.sh
@@ -3,4 +3,5 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd git-repo
-/checkmarx/runCxConsole.sh Scan -v -cxserver $host -projectname "$project" -cxuser $username -cxpassword $password -locationtype $locationType -locationpath $locationPath
+sourcePath=`pwd`/$locationPath
+/checkmarx/runCxConsole.sh Scan -v -cxserver $host -projectname "$project" -cxuser $username -cxpassword $password -locationtype $locationType -locationpath $sourcePath
